92293fbb40111fa86a8bbdcdce146bc45604caab
bugerset <youngseo1114@naver.com>
2022-08-20 10:41:54 +0900

    1

--- 초급 1/1/1212.py
+++ 초급 1/1/1212.py
@@ -1,3 +1,26 @@
 # 1212 8진수 2진수
-a = input()
-print(bin(int(a,8))[2:])
+
+n = int(input())
+a = []
+
+while n!=0:
+    if n%-2 == -1:
+        a.append("1")
+        n = (n//-2)+1
+    else:
+        a.append("0")
+        n = n//-2
+
+
+a.reverse()
+if len(a) > 0:
+    print("".join(a))
+else:
+    print(0)
+
+# -13/-2  -2*7+1
+# 7/-2    -2*-3+1
+# -3/-2    -2*2+1
+# 2/-2     -2*-1+0
+# -1/-2    -2*1+1
+# 1/2      -2*0+1
---
+++ 초급 1/1/2089.py
@@ -0,0 +1,4 @@
+# 2089 -2진수
+
+a = int(input())
+print(bin(a))
