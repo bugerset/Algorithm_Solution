c70d70352f566f0fd28d52088e87349828d29d63
bugerset <youngseo1114@naver.com>
2022-09-04 16:35:10 +0900

    dp

--- 초급 1/1/연습장.py
+++ 초급 1/1/연습장.py
@@ -1,1 +1,15 @@
-print(1.1 + 1.1 == 2.2)
+# dp공부
+n = int(input())
+food = list(map(int, input().split()))
+
+# 작은 문제부터 해결해서 저장할 dp테이블
+dp = [0]*100
+
+# 다이나믹 프로그래밍(bottom up)
+dp[0] = food[0]
+dp[1] = food[1]
+
+for i in range(n):
+    dp[i]=max(dp[i-1], dp[i-2]+food[i])
+
+print(dp[n-1])
